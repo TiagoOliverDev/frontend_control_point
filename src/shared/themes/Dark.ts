005116e4f03659f095cff12f7cdf56b5a5764f86
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-22 21:54:53 -0300

    feat: configurando tema

---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,32 @@
+import { createTheme } from "@mui/material";
+import { cyan, yellow } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: yellow[700],
+            dark: yellow[800],
+            light: yellow[500],
+            contrastText: "#FFFFFF",
+        },
+        
+        secondary: {
+            main: cyan[500],
+            dark: cyan[400],
+            light: cyan[300],
+            contrastText: "#FFFFFF",
+        },
+        
+        background: {
+            default: "#202124",
+            paper: "#303234",
+        },
+    },
+
+    typography: {
+        allVariants: {
+            color: "#FFFFFF"
+        },
+    },
+});
