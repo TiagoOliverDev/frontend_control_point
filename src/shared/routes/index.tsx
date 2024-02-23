824b2213d37e0b6376f455167421447f8c360a26
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-22 22:32:15 -0300

    feat: configurando routes

---
+++ src/shared/routes/index.tsx
@@ -0,0 +1,79 @@
+import { useEffect } from "react";
+
+import { Routes, Route, Navigate } from "react-router-dom"
+
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import PeopleIcon from '@mui/icons-material/People';
+import HomeIcon from '@mui/icons-material/Home';
+import DriveFileMoveIcon from '@mui/icons-material/DriveFileMove';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
+
+import { useAppDrawerContext } from "../contexts";
+
+import { Dashboard, ListCollaborators, CollaboratorsDetails, ListSectors, SectorDetails, Reports, Ponto } from "../../pages";
+
+export const AppRoutes = () => {
+
+    const { setDrawerOption } = useAppDrawerContext();
+
+    useEffect(() => {
+        
+        setDrawerOption([
+            {
+                icon: <HomeIcon />,
+                path: "/",
+                label: "Página Inicial",
+            },
+            {
+                icon: <AnalyticsIcon />,
+                path: "/dashboard",
+                label: "Dashboard",
+            },
+            {
+                icon: <AccountBalanceIcon />,
+                path: "/sectors",
+                label: "Cadastrar setores",
+            },
+            {
+                icon: <PeopleIcon />,
+                path: "/collaborators",
+                label: "Cadastrar colaborador",
+            },
+            {
+                icon: <PendingActionsIcon />,
+                path: "/ponto",
+                label: "Ponto",
+            }, 
+            {
+                icon: <DriveFileMoveIcon />,
+                path: "/reports",
+                label: "Relatórios",
+            },    
+      
+
+        ]);
+    }, [setDrawerOption]);
+
+    return (
+        <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+
+            <Route path="/" element={<Dashboard />} />
+
+            <Route path="/collaborators" element={<ListCollaborators />} />
+            <Route path="/persons/details/:id" element={<CollaboratorsDetails />} />
+            
+            <Route path="/sectors" element={<ListSectors />} />
+            <Route path="/sector/details/:id" element={<SectorDetails />} />
+
+            <Route path="/reports" element={<Reports />} />
+
+            <Route path="/ponto" element={<Ponto />} />
+
+            <Route path="*" element={<Navigate to={"/login"} />} />
+        </Routes>
+    );
+};
