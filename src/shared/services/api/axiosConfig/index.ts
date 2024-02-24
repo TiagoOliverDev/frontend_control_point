4d1e179227b5c2d35f30af7a34cb750822330836
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-24 12:39:57 -0300

    feat: headers com bearer token para cada requisição no back. Comentado por enquanto.

--- src/shared/services/api/axiosConfig/index.ts
+++ src/shared/services/api/axiosConfig/index.ts
@@ -5,6 +5,10 @@ import { Enviroment } from "../../../environment";
 
 const API = axios.create({
     baseURL: Enviroment.URL_BASE,
+    // headers: {
+    //     // Enviando JWT do local storage para cada requisição que for feito no back
+    //     Authorization: `Bearer ${JSON.parse(localStorage.getItem('APP_ACCESS_TOKEN') || '' )}`,
+    // }
 });
 
 API.interceptors.response.use(
