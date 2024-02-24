7d17ad185b9f55ba0601bf1c81b1333f02e8e1d2
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-23 23:06:39 -0300

    feat: criando context auth

---
+++ src/shared/contexts/AuthContext.tsx
@@ -0,0 +1,59 @@
+import { createContext, useCallback, useEffect, useMemo, useState } from "react"
+import { AuthService } from "../services/api/auth/AuthService";
+
+interface IAuthCOntextData {
+    login: (email: string, password: string  )=> Promise<string | void>
+    isAuthenticated: boolean;
+    logout: () => void; // não precisa ser Promise pois não vai se conectar com o back
+}
+
+interface IAuthProvideProps{
+    children: React.ReactNode;
+}
+
+const AuthContext = createContext({} as IAuthCOntextData ) // injetando as props de IAuthCOntextData no context
+
+const LOCAL_STORAGE_KEY_ACCESS_TOKEN = 'APP_ACCESS_TOKEN'
+
+export const AuthProvider: React.FC<IAuthProvideProps> = ({ children }) => {
+    const [accessToken, setAccessToken] = useState<string>()
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN);
+
+        if (accessToken){
+            setAccessToken(JSON.parse(accessToken))
+        } else {
+            setAccessToken(undefined);
+        }
+
+    })
+
+    const handleLogin = useCallback(async (email: string, password: string ) => {
+        const result = await AuthService.auth(email, password);
+
+        if (result instanceof Error){
+            return result.message
+        } else {          
+            localStorage.setItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN, JSON.stringify(result.accessToken)) // Salvando token no local storage
+            setAccessToken(result.accessToken)
+        }
+
+    }, []);
+
+    const handleLogout = useCallback(() => {
+        localStorage.removeItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN)
+        setAccessToken(undefined)
+        
+    }, []);
+    
+    const isAuthenticated = useMemo(() => accessToken !== undefined, [accessToken])
+
+
+    return (
+        // Funções passadas em contexto obrigatoriamente tem que usar o useCallback para não prejudicar o desempenho
+        <AuthContext.Provider value={{ login: handleLogin, isAuthenticated, logout: handleLogout }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
