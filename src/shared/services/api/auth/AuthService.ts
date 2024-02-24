cc3d643de98cc6a9621710732f0f865e83e42a56
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-23 23:06:57 -0300

    feat: criando auth service

---
+++ src/shared/services/api/auth/AuthService.ts
@@ -0,0 +1,30 @@
+import { API } from "../axiosConfig";
+
+interface IAuth {
+    accessToken: string;
+}
+
+
+const auth = async (email: string, password: string): Promise<IAuth | Error> => {
+    try {
+
+        // Na api do backend mesmo deve ser POST, está GET somente por conta do mock
+        // const { data } = await API.post<IDetailsSetor>("/auth", { data:{email,password} });
+        const { data } = await API.get("/auth", { data:{email,password} });
+        
+        if (data) {
+            return data;
+        };
+
+        return new Error("Erro no login.");
+    } catch (error) {
+        console.error(error);
+        return new Error((error as { message: string }).message || "Erro no login..");
+    };
+};
+
+
+
+export const AuthService = {
+    auth,
+};
