91d7287d7fd1af1a0a6d490352cab4519a1ae795
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-25 13:24:15 -0300

    feat: alterando lógica de auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./shared/components/forms/TranslationYup";
 
-import { AppDrawerProvider, AppThemeProvider, AuthProvider } from "./shared/contexts";
+import { AppDrawerProvider, AppThemeProvider, AuthProvider, useAuthContext } from "./shared/contexts";
 import { AppRoutes } from "./shared/routes";
 import { Login, MenuDrawer } from "./shared/components";
+import { Register } from "./pages"; 
 
 
 export const App = () => {
   return (
     <AuthProvider>
-      <AppThemeProvider >
-
-        <Login>
-
-          <AppDrawerProvider >
-
-            <BrowserRouter>
-            
-              <MenuDrawer>
-                <AppRoutes />
-              </MenuDrawer>
-
-            </BrowserRouter >
-
-          </AppDrawerProvider>
-
-        </Login>
-
+      <AppThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/*" element={<LoginWrapper />} />
+          </Routes>
+        </BrowserRouter>
       </AppThemeProvider>
     </AuthProvider>
   );
+};
+
+const LoginWrapper = () => {
+  const { isAuthenticated }: { isAuthenticated: boolean } = useAuthContext();
+
+  if (isAuthenticated) {
+    return (
+      <AppDrawerProvider>
+        <MenuDrawer>
+          <AppRoutes />
+        </MenuDrawer>
+      </AppDrawerProvider>
+    );
+  } else {
+    return <Login />;
+  }
 };
