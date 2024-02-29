aca88afc0ee7bc7b771a44fa756bdb88b173ac35
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-29 01:16:54 -0300

    feat: types

---
+++ src/@types/IListPoints.ts
@@ -0,0 +1,13 @@
+export interface IListsPoint {
+    id: number;
+    idCollaborator: number;
+    tipoCadastro: string;
+    data: string,
+    nomeCompleto: string;
+    entrada: string;
+    pausaAlmoco: string;
+    retornoAlmoco: string;
+    saidaEsporadica: string;
+    retornoEsporadica: string;
+    fimExpediente: string;
+};
